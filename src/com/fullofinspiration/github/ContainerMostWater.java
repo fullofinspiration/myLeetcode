d529b1371061af31103fae814be98335293853f2
zhangzhenpeng <zhenpeng0506@163.com>
2018-04-11 16:03:18 +0800

    最大面积，z字形

--- src/com/fullofinspiration/github/ContainerMostWater.java
+++ src/com/fullofinspiration/github/ContainerMostWater.java
@@ -1,4 +1,28 @@
 package com.fullofinspiration.github;
 
+/**
+ * 看如何能盛下最多的水
+ * 我是参考了hint中才实现的，原来写的时间复杂度是O(n^2),参考的这篇时间复杂度是O(n)
+ */
 public class ContainerMostWater {
+    public int maxArea(int[] height) {
+        int maxValue = 0;
+        int i = 0, j = height.length - 1;
+        while (i < j) {
+            maxValue = Math.max(maxValue, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxValue;
+    }
+
+    public static void main(String[] args) {
+        ContainerMostWater cms = new ContainerMostWater();
+        int[] a = {1, 2, 1};
+        int b = cms.maxArea(a);
+        System.out.println(b);
+    }
 }
--- src/com/fullofinspiration/github/ZigzagConversion.java
+++ src/com/fullofinspiration/github/ZigzagConversion.java
@@ -1,4 +1,39 @@
 package com.fullofinspiration.github;
 
+import java.util.ArrayList;
+
+/**
+ * z字形转换
+ */
 public class ZigzagConversion {
+    public String convert(String s, int numRows) {
+        char[] c = s.toCharArray();
+        int len = c.length;
+        StringBuilder[] sb = new StringBuilder[numRows];
+        for (int i = 0; i <sb.length; i++) {
+            sb[i] = new StringBuilder();
+        }
+        int i = 0;
+        while (i < len) {
+            for (int j = 0; j < numRows && i < len; j++) {
+                sb[j].append(c[i++]);
+            }
+            for (int j = numRows - 2; j > 0 && i < len; j--) {
+                sb[j].append(c[i++]);
+            }
+        }
+
+        StringBuilder sb1 = new StringBuilder();
+        for (int j = 0; j < sb.length; j++) {
+            sb1.append(sb[j]);
+        }
+        return sb1.toString();
+    }
+
+    public static void main(String[] args) {
+        ZigzagConversion zc = new ZigzagConversion();
+        String s = "PAYPALISHIRING";
+        String s1 = zc.convert(s, 3);
+        System.out.println(s1);
+    }
 }
