9ce14a0f7ad8aa12bc0e02f86cb445ad449450f6
zhangzhenpeng <zhangzhenpeng@itiger.com>
2021-02-21 15:58:02 +0800

    链表排序增加官网解法以供参考

--- src/main/java/com/fullofinspiration/github/leetcode/_0148SortList.java
+++ src/main/java/com/fullofinspiration/github/leetcode/_0148SortList.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
  * Created by Zhenpeng Zhang on 2021/2/19.
  */
 public class _0148SortList {
-
     public static void main(String[] args) {
         ListNode listNode = buildList(new Integer[]{3, 2, 4});
         Solution solution = new _0148SortList().new Solution();
@@ -85,7 +84,6 @@ public class _0148SortList {
         }
     }
 
-
     private static ListNode buildList(Integer[] vals) {
         List<ListNode> list = Arrays.stream(vals).map(ListNode::new).collect(Collectors.toList());
         for (int i = 0; i < list.size(); i++) {
@@ -103,5 +101,104 @@ public class _0148SortList {
             listNode = listNode.next;
         }
         System.out.println();
+    }
+}
+
+//leetcode上的解法，我是参考这里面的思想解出来的, 和我的解法区别不大，有时间可以看下
+//https://leetcode.com/problems/sort-list/discuss/46767/Java-solution-with-strict-O(1)-auxiliary-space-complexity
+class Solution1 {
+    private class MergeHelper {
+        public ListNode newHead;
+        public ListNode newTail;
+    }
+    public ListNode sortList(ListNode head) {
+        if ( head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummyHeadOne = new ListNode(0);
+        ListNode dummyHeadTwo = new ListNode(0);
+        ListNode dummySortedHead = new ListNode(0);
+        ListNode dummySortedLast = dummySortedHead;
+        ListNode unvisitedNode = head;
+        MergeHelper mergeRst = new MergeHelper();
+
+        int listLength = 0;
+        int level = 0;
+        while ( unvisitedNode != null && unvisitedNode.next != null ) {
+            unvisitedNode = addNode ( dummyHeadOne, unvisitedNode, 1<<level);
+            unvisitedNode = addNode ( dummyHeadTwo, unvisitedNode, 1<<level);
+            merge ( dummyHeadOne.next, dummyHeadTwo.next, mergeRst);
+            dummySortedLast.next = mergeRst.newHead;
+            dummySortedLast = mergeRst.newTail;
+            listLength += 2;
+        }
+        if (unvisitedNode != null) {
+            dummySortedLast.next = unvisitedNode;
+            listLength ++;
+        }
+        level ++;
+
+        while ( listLength > 1 << level) {
+            dummySortedLast = dummySortedHead;
+            unvisitedNode = dummySortedHead.next;
+            while (unvisitedNode != null) {
+                unvisitedNode = addNode ( dummyHeadOne, unvisitedNode, 1<<level);
+                unvisitedNode = addNode ( dummyHeadTwo, unvisitedNode, 1<<level);
+                merge ( dummyHeadOne.next, dummyHeadTwo.next, mergeRst);
+                dummySortedLast.next = mergeRst.newHead;
+                dummySortedLast = mergeRst.newTail;
+            }
+            level ++;
+        }
+
+        return dummySortedHead.next;
+    }
+
+    /* merge listOne and listTwo.
+    Save the sorted list head into rst.newHead
+    Save the last node of the sorted list into rst.newTail
+    */
+    private void merge (ListNode listOne, ListNode listTwo, MergeHelper rst) {
+        ListNode dummyHead = new ListNode (0);
+        ListNode lastNode = dummyHead;
+        while (listOne != null && listTwo != null) {
+            if ( listOne.val < listTwo.val ) {
+                lastNode.next = listOne;
+                listOne = listOne.next;
+            } else {
+                lastNode.next = listTwo;
+                listTwo = listTwo.next;
+            }
+            lastNode = lastNode.next;
+        }
+
+        while (listOne != null) {
+            lastNode.next = listOne;
+            listOne = listOne.next;
+            lastNode = lastNode.next;
+        }
+        while ( listTwo != null ) {
+            lastNode.next = listTwo;
+            listTwo = listTwo.next;
+            lastNode = lastNode.next;
+        }
+        rst.newHead = dummyHead.next;
+        rst.newTail = lastNode;
+    }
+
+    /*
+     add at max #"count" nodes into "head" from "source"
+     return the new position of source after adding.
+    */
+    private ListNode addNode ( ListNode head, ListNode source, int count ) {
+        while (count > 0 && source != null) {
+            head.next = source;
+            head = head.next;
+            source = source.next;
+            count --;
+        }
+        head.next = null;
+        return source;
     }
 }
