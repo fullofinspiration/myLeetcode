fd344d590e2d57e8bb7b16588abf81c39b3b9a36
zhangzhenpeng <zhangzhenpeng@itiger.com>
2021-08-29 23:02:21 +0800

    超时，后面再做

--- src/main/java/com/fullofinspiration/github/leetcode/_0148SortList.java
+++ src/main/java/com/fullofinspiration/github/leetcode/_0148SortList.java
@@ -10,14 +10,48 @@ import java.util.stream.Collectors;
  * Created by Zhenpeng Zhang on 2021/2/19.
  */
 public class _0148SortList {
+    //todo 目前超时，后面再做
+    class Solution {
+        //冒泡
+        public ListNode sortList(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+            ListNode dummy = new ListNode();
+            ListNode preOfBegin = new ListNode();
+            preOfBegin.next = head;
+            while (preOfBegin.next != null) {
+                ListNode curMax = preOfBegin.next;
+                ListNode preOfMax = preOfBegin;
+                ListNode curNode = curMax;
+                ListNode preOfCurNode = preOfMax;
+                while (curNode != null) {
+                    if (curNode.val > curMax.val) {
+                        curMax = curNode;
+                        preOfMax = preOfCurNode;
+                    }
+                    preOfCurNode = curNode;
+                    curNode = curNode.next;
+                }
+                preOfMax.next = curMax.next;
+                curMax.next = dummy.next;
+                dummy.next = curMax;
+                if (preOfBegin.next == curMax) {
+                    preOfBegin.next = curMax.next;
+                }
+            }
+            return dummy.next;
+        }
+    }
+
     public static void main(String[] args) {
         ListNode listNode = buildList(new Integer[]{3, 2, 4});
-        Solution solution = new _0148SortList().new Solution();
+        Solution1 solution = new _0148SortList().new Solution1();
         ListNode result = solution.sortList(listNode);
         print(result);
     }
 
-    class Solution {
+    class Solution1 {
 
         public ListNode sortList(ListNode head) {
             if (head == null || head.next == null) {
@@ -113,8 +147,9 @@ class Solution1 {
         public ListNode newHead;
         public ListNode newTail;
     }
+
     public ListNode sortList(ListNode head) {
-        if ( head == null || head.next == null) {
+        if (head == null || head.next == null) {
             return head;
         }
 
@@ -127,31 +162,31 @@ class Solution1 {
 
         int listLength = 0;
         int level = 0;
-        while ( unvisitedNode != null && unvisitedNode.next != null ) {
-            unvisitedNode = addNode ( dummyHeadOne, unvisitedNode, 1<<level);
-            unvisitedNode = addNode ( dummyHeadTwo, unvisitedNode, 1<<level);
-            merge ( dummyHeadOne.next, dummyHeadTwo.next, mergeRst);
+        while (unvisitedNode != null && unvisitedNode.next != null) {
+            unvisitedNode = addNode(dummyHeadOne, unvisitedNode, 1 << level);
+            unvisitedNode = addNode(dummyHeadTwo, unvisitedNode, 1 << level);
+            merge(dummyHeadOne.next, dummyHeadTwo.next, mergeRst);
             dummySortedLast.next = mergeRst.newHead;
             dummySortedLast = mergeRst.newTail;
             listLength += 2;
         }
         if (unvisitedNode != null) {
             dummySortedLast.next = unvisitedNode;
-            listLength ++;
+            listLength++;
         }
-        level ++;
+        level++;
 
-        while ( listLength > 1 << level) {
+        while (listLength > 1 << level) {
             dummySortedLast = dummySortedHead;
             unvisitedNode = dummySortedHead.next;
             while (unvisitedNode != null) {
-                unvisitedNode = addNode ( dummyHeadOne, unvisitedNode, 1<<level);
-                unvisitedNode = addNode ( dummyHeadTwo, unvisitedNode, 1<<level);
-                merge ( dummyHeadOne.next, dummyHeadTwo.next, mergeRst);
+                unvisitedNode = addNode(dummyHeadOne, unvisitedNode, 1 << level);
+                unvisitedNode = addNode(dummyHeadTwo, unvisitedNode, 1 << level);
+                merge(dummyHeadOne.next, dummyHeadTwo.next, mergeRst);
                 dummySortedLast.next = mergeRst.newHead;
                 dummySortedLast = mergeRst.newTail;
             }
-            level ++;
+            level++;
         }
 
         return dummySortedHead.next;
@@ -161,11 +196,11 @@ class Solution1 {
     Save the sorted list head into rst.newHead
     Save the last node of the sorted list into rst.newTail
     */
-    private void merge (ListNode listOne, ListNode listTwo, MergeHelper rst) {
-        ListNode dummyHead = new ListNode (0);
+    private void merge(ListNode listOne, ListNode listTwo, MergeHelper rst) {
+        ListNode dummyHead = new ListNode(0);
         ListNode lastNode = dummyHead;
         while (listOne != null && listTwo != null) {
-            if ( listOne.val < listTwo.val ) {
+            if (listOne.val < listTwo.val) {
                 lastNode.next = listOne;
                 listOne = listOne.next;
             } else {
@@ -180,7 +215,7 @@ class Solution1 {
             listOne = listOne.next;
             lastNode = lastNode.next;
         }
-        while ( listTwo != null ) {
+        while (listTwo != null) {
             lastNode.next = listTwo;
             listTwo = listTwo.next;
             lastNode = lastNode.next;
@@ -193,12 +228,12 @@ class Solution1 {
      add at max #"count" nodes into "head" from "source"
      return the new position of source after adding.
     */
-    private ListNode addNode ( ListNode head, ListNode source, int count ) {
+    private ListNode addNode(ListNode head, ListNode source, int count) {
         while (count > 0 && source != null) {
             head.next = source;
             head = head.next;
             source = source.next;
-            count --;
+            count--;
         }
         head.next = null;
         return source;
---
+++ src/test/java/com/fullofinspiration/github/leetcode/_0148SortListTest.java
@@ -0,0 +1,38 @@
+package com.fullofinspiration.github.leetcode;
+
+import com.fullofinspiration.github.tool.ListNode;
+import com.fullofinspiration.github.util.ListNodeUtil;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class _0148SortListTest {
+    private static final _0148SortList.Solution SOLUTION = new _0148SortList().new Solution();
+
+    @Test
+    public void test00() {
+        ListNode listNode = ListNodeUtil.buildListNode(new int[]{4, 2, 1, 3});
+        ListNode res = SOLUTION.sortList(listNode);
+        ListNode expect = ListNodeUtil.buildListNode(new int[]{1, 2, 3, 4});
+        boolean equal = ListNodeUtil.equal(expect, res);
+        assertTrue(equal);
+    }
+
+    @Test
+    public void test01() {
+        ListNode listNode = ListNodeUtil.buildListNode(new int[]{-1, 5, 3, 4, 0});
+        ListNode res = SOLUTION.sortList(listNode);
+        ListNode expect = ListNodeUtil.buildListNode(new int[]{-1, 0, 3, 4, 5});
+        boolean equal = ListNodeUtil.equal(expect, res);
+        assertTrue(equal);
+    }
+
+    @Test
+    public void test02() {
+        ListNode listNode = ListNodeUtil.buildListNode(new int[]{});
+        ListNode res = SOLUTION.sortList(listNode);
+        ListNode expect = ListNodeUtil.buildListNode(new int[]{});
+        boolean equal = ListNodeUtil.equal(expect, res);
+        assertTrue(equal);
+    }
+}
