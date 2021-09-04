a7651b426bbbe104ff0d8d51746993430cffc0fa
zhangzhenpeng <zhangzhenpeng@itiger.com>
2021-09-05 01:41:45 +0800

    删除错误的提交

--- src/main/java/com/fullofinspiration/github/leetcode/_0101_SymmetricTree.java
+++ src/main/java/com/fullofinspiration/github/leetcode/_0101_SymmetricTree.java
@@ -6,40 +6,6 @@ import java.util.LinkedList;
 
 /**
  * @formatter:off
- * if (root == null)  {
- *     return true;
- * return true;
- * }
- * List<Node> all = new ArrayList<>();
- * all.add(root);
- * while(true) {
- *     int startIndex=0;
- *     int endIndex=all.size()-1;
- *     for(;startIndex <endIndex; startIndex++,eneIndex--){
- *         if ( (all[startIndex].left==null && all[endIndex].right!=null) ||
- *             (all[startIndex].left!=null && all[endIndex].right == null) ||
- *         all[startIndex].value != all[endIndex].value) {
- *             return false;
- *         }
- *     }
- *     intNextNotNullCount = 0;
- *     List<Node> nextNodes = new ArrayList();
- *     for(int i=0, i< all.size();i++) {
- *        if (all.get(i) == null) {
- *            nextNodes.add(null);
- *            nextNodes.add(null);
- *        } else {
- *           intNextNotNullCount++;
- *           Node curNode =  all.get(i);
- *           nextNodes.add(curNode.left);
- *           nextNodes.add(curNode.right);
- *        }
- *     }
- *     //说明下一层都没有值，表明是对称的
- *     if (intNextNotNullCount == 0) {
- *         return true;
- *     }
- *     all= nextNodes;
  * @formatter:on
  */
 public class _0101_SymmetricTree {
