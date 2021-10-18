28737b38e7635d19bdd33f6de2b26b0efd3bac63
zhangzhenpeng <zhangzhenpeng@itiger.com>
2021-10-19 00:27:03 +0800

    200 岛屿问题第二次写

--- src/main/java/com/fullofinspiration/github/leetcode/_200_NumberOfLands.java
+++ src/main/java/com/fullofinspiration/github/leetcode/_200_NumberOfLands.java
@@ -5,7 +5,56 @@ import java.util.Objects;
 import java.util.Set;
 
 public class _200_NumberOfLands {
+    /**
+     * 新建一个数组，设置每个节点是否被访问过
+     * 一个总数量标记当前岛屿数量
+     * 从左到右 从上到下:
+     * 若一个节点为0，遍历下一个节点
+     * 若一个节点访问过，遍历下一个节点
+     * 已当前节点为中心，上下左右，穷举所有能直接访问的1的节点，然后将总数加1
+     * 穷举过程：
+     * 递归的方式，上下左右（必须上下左右，否则有可能漏掉值）
+     */
     class Solution {
+        boolean[][] visited;
+
+        public int numIslands(char[][] grid) {
+            if (grid.length == 0) {
+                return 0;
+            }
+            visited = new boolean[grid.length][grid[0].length];
+            int nums = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == '0' || visited[i][j]) {
+                        continue;
+                    }
+                    traverse(i, j, grid);
+                    nums++;
+                }
+            }
+            return nums;
+        }
+
+        private void traverse(int row, int col, char[][] grid) {
+            if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+                return;
+            }
+            if (visited[row][col]) {
+                return;
+            }
+            if (grid[row][col] == '0') {
+                return;
+            }
+            visited[row][col] = true;
+            traverse(row - 1, col, grid);
+            traverse(row + 1, col, grid);
+            traverse(row, col - 1, grid);
+            traverse(row, col + 1, grid);
+        }
+    }
+
+    class Solution1 {
         private Set<Key> visitedKeys = new HashSet<>();
 
         public int numIslands(char[][] grid) {
---
+++ src/test/java/com/fullofinspiration/github/knifeoffer/_051_ReversePairSize.java
@@ -0,0 +1,16 @@
+package com.fullofinspiration.github.knifeoffer;
+
+/**
+ * hard
+ * 太难了 不想了
+ * 第一个开始
+ * f(i) = f(i-1) num[i]>= num[i-1]
+ *
+ */
+public class _051_ReversePairSize {
+    class Solution {
+        public int reversePairs(int[] nums) {
+            return 0;
+        }
+    }
+}
