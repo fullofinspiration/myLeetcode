27da8bbcde1526f426d38e895cd990a121d0057e
zhangzhenpeng <zhangzhenpeng@itiger.com>
2021-10-27 21:24:16 +0800

    滴滴面试

---
+++ src/main/java/com/fullofinspiration/github/interview/BinarySearcher.java
@@ -0,0 +1,22 @@
+package com.fullofinspiration.github.interview;
+
+/**
+ * 滴滴
+ */
+public class BinarySearcher {
+    public int binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/com/fullofinspiration/github/interview/BinaryTreeAdderOfOdd.java
@@ -0,0 +1,61 @@
+package com.fullofinspiration.github.interview;
+
+import java.util.LinkedList;
+
+/**
+ * 滴滴
+ */
+public class BinaryTreeAdderOfOdd {
+    public int sumOddOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int rst = 0;
+        int total = 1;
+        LinkedList<TreeNode> all = new LinkedList<>();
+        all.add(root);
+        while (!all.isEmpty()) {
+            int nums = all.size();
+            while (nums > 0) {
+                TreeNode curNode = all.removeLast();
+                nums--;
+                if (curNode.left != null) {
+                    all.addFirst(curNode.left);
+                    total++;
+                    if (total % 2 == 0) {
+                        rst += curNode.left.val;
+                    }
+                }
+                if (curNode.right != null) {
+                    total++;
+                    all.addFirst(curNode.right);
+                    if (total % 2 == 0) {
+                        rst += curNode.right.val;
+                    }
+                }
+            }
+        }
+        return rst;
+    }
+
+
+    public class TreeNode {
+
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
